0a1d1519f59d1b5ecaa235dc581e71076139b30f
Jelmer Cormont <jelmer@cormont.work>
2023-10-18 20:55:29 +0800

    Use standard npm version command with script

---
+++ scripts/copy-version.js
@@ -0,0 +1,35 @@
+import * as fs from "node:fs";
+
+const MAIN_PACKAGE = "@desk-framework/frame-core";
+const PACKAGES = [
+	"packages/frame-core",
+	"packages/frame-test",
+	"packages/frame-web",
+];
+
+// get current version
+let packageJson = fs.readFileSync("package.json", "utf-8");
+let packageJsonObj = JSON.parse(packageJson);
+let currentVersion = packageJsonObj.version;
+if (packageJsonObj.name !== "desk") {
+	throw new Error("Not running in root package directory");
+}
+
+// determine publish tag
+let isPre = /[^\d\.]/.test(currentVersion);
+let newPublishTag = isPre ? "next" : "latest";
+
+// set package versions
+for (let packagePath of PACKAGES) {
+	let packageJson = fs.readFileSync(`${packagePath}/package.json`, "utf-8");
+	let packageJsonObj = JSON.parse(packageJson);
+	packageJsonObj.version = currentVersion;
+	packageJsonObj.publishConfig = { tag: newPublishTag };
+	if (packageJsonObj.name !== MAIN_PACKAGE) {
+		packageJsonObj.peerDependencies[MAIN_PACKAGE] = currentVersion;
+	}
+	fs.writeFileSync(
+		`${packagePath}/package.json`,
+		JSON.stringify(packageJsonObj, null, "\t") + "\n",
+	);
+}
