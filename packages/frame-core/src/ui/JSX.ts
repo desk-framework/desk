bafe7889b17347245482b38d0dd1938e6a442976
Jelmer Cormont <jelmer@cormont.work>
2024-03-02 16:30:22 +0800

    Include definitions for JSX intrinsic tags

--- packages/frame-core/src/ui/JSX.ts
+++ packages/frame-core/src/ui/JSX.ts
@@ -4,10 +4,18 @@ import {
 	LazyString,
 	strf,
 	StringFormatBinding,
+	ui,
 	View,
 	ViewClass,
 	ViewComposite,
 } from "../index.js";
+
+/** Tag names with `ui` function names that are different */
+const tagNames: any = {
+	animatedcell: "animatedCell",
+	textfield: "textField",
+	render: "renderView",
+};
 
 /** Helper function to flatten component arrays */
 function flatten(a: any[]): any {
@@ -18,24 +26,8 @@ function flatten(a: any[]): any {
 	return result;
 }
 
-/**
- * JSX support for UI components
- *
- * @summary This function provides support for JSX elements, when used in `.jsx` or `.tsx` files. it's used by the TypeScript compiler (or any other JSX compiler) to convert JSX elements to function calls.
- *
- * @note This function is also available as `ui.JSX`. For TypeScript projects, set the `jsxFactory` configuration to `ui.JSX`, and import `ui` in each `.tsx` file to allow the compiler to access it.
- *
- * **Bindings in label text** — Several JSX elements accept text content, namely `<ui.label>`, `<ui.button>`, `<ui.toggle>`, and `<ui.textfield>` (for placeholder text). This text is assigned to the `text`, `label`, or `placeholder` properties, and may consist of plain text and bindings (i.e. the result of {@link bound()} functions, refer to {@link Binding} for more information).
- *
- * As a shortcut within JSX text content, bindings may also be specified directly using `%[...]` syntax, which are used with {@link bound.strf()}. In addition, this syntax may be used with aliases and default string values:
- *
- * - `Foo: %[foo]` — inserts a binding for `foo`
- * - `Foo: %[foo:.2f]` — inserts a binding for `foo`, and uses {@link bound.strf()} to format the bound value as a number with 2 decimals
- * - `Foo: %[foo=somePropertyName]` — inserts a binding for `somePropertyName`, but allows for localization of `Foo: %[foo]` instead
- * - `Foo: %[foo=another.propertyName:.2f]` — inserts a bindings for `another.propertyName`, but allows for localization of `Foo: %[foo:.2f]` instead.
- * - `Foo: %[foo=some.numProp:?||None]` — inserts a binding for `some.numProp`, but allows for localization of `Foo: %[foo:?||None]` instead (and inserts `None` if the value for `some.numProp` is undefined or an empty string).
- */
-export function JSX(f: any, presets: any, ...rest: any[]): ViewClass {
+/** @internal JSX implementation */
+export function jsx(tag: any, presets: any, ...rest: any[]): ViewClass {
 	rest = flatten(rest);
 
 	// use string content as 'text' property, if any
@@ -87,7 +79,8 @@ export function JSX(f: any, presets: any, ...rest: any[]): ViewClass {
 	}
 
 	// invoke function with merged presets and content
-	if (typeof f !== "function") throw err(ERROR.JSX_InvalidTag, String(f));
+	let f = typeof tag === "string" ? (ui as any)[tagNames[tag] || tag] : tag;
+	if (typeof f !== "function") throw err(ERROR.JSX_InvalidTag, String(tag));
 	if (f.prototype instanceof ViewComposite) {
 		return class extends f {
 			constructor() {
@@ -98,21 +91,4 @@ export function JSX(f: any, presets: any, ...rest: any[]): ViewClass {
 		throw err(ERROR.JSX_InvalidTag, f.name);
 	}
 	return f(merged, ...components);
-}
-
-export namespace JSX {
-	/** TypeScript JSX typing information */
-	export namespace JSX {
-		/**
-		 * Type definition for intrinsic elements (used by TypeScript compiler)
-		 * - Refer to {@link JSX} for more information.
-		 */
-		export type IntrinsicElements = {};
-
-		/**
-		 * Type definition for the result of a JSX call (used by TypeScript compiler)
-		 * - Refer to {@link JSX} for more information.
-		 */
-		export type Element = ViewClass;
-	}
 }
